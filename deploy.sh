85729c211b077b3c3c11df6a98793e9dcb23bfd7
Vaidas Jablonskis <jablonskis@gmail.com>
2015-12-13 21:47:20 +0000

    stuff

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+set -e
+
+sed -i "s/%TAG%/%{CIRCLE_TAG}" kube/helloworld-rc.yaml
+
+kubectl --username=${KUBE_USER} --password=${KUBE_PASS} \
+  --insecure-skip-tls-verify --server=${KUBE_SERVER} \
+  --namespace=${KUBE_NAMESPACE} delete rc/helloworld
+
+kubectl --username=${KUBE_USER} --password=${KUBE_PASS} \
+  --insecure-skip-tls-verify --server=${KUBE_SERVER} \
+  --namespace=${KUBE_NAMESPACE} create -f kube/helloworld-rc.yaml
